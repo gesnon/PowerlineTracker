3620cc464c106c619b123f91bf1998b5ce649545
Sergey Panktov <gesnon@gmail.com>
2020-04-03 20:02:30 +0400

    Удалил ненужные вкладки и сделал главной страницей список объектов

--- PowerlineTracker/App_Start/RouteConfig.cs
+++ PowerlineTracker/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace PowerlineTracker
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Powerline", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
